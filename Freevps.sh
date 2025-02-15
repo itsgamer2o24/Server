1c56d973725d7b325080f44ae0d8a07c68e50dfa
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-14 19:31:12 -0800

    Update Freevps.sh

--- Freevps.sh
+++ Freevps.sh
@@ -37,16 +37,18 @@ apt install wget
 apt install curl
 clear
 if [ $? -ne 0 ]; then
-    echo -e "${RED}[ERROR] Failed to install dependencies.\033[0m"
+    echo -e "${RED}[ERROR] Failed to install dependencies.${NC}"
     exit 1
 fi
 
 # Clone the repository and enter the directory
-echo -e "${BLUE}[INFO] Cloning FreeVPS repository...\033[0m"
-wget https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/Freevps_install.sh && pwd
+echo -e "${BLUE}[INFO] Cloning FreeVPS repository...${NC}"
+wget https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/Freevps_install.sh 
+sleep 20
+pwd
 nano Freevps_install.sh
-Wait 20
+sleep 20
 chmod +x Freevps_install.sh
-./Freevps.sh || { echo -e "\033[1;31m[ERROR] Failed to enter the FreeVPS directory.\033[0m"; exit 1; }
+./Freevps.sh || { echo -e "${RED}[ERROR] Failed to enter the FreeVPS directory.${NC}"; exit 1; }
 
-echo -e "\033[1;32m[INFO] WorldCraftLink Panel setup complete!\033[0m"
+echo -e "${BLUR}[INFO] FreeVPS setup complete!${NC}"
