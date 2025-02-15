5ec9d583a728d7e275de32c3eb47a829971630d7
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-14 19:25:09 -0800

    Update Freevps.sh

--- Freevps.sh
+++ Freevps.sh
@@ -1,83 +1,52 @@
-#!/bin/sh
-
-ROOTFS_DIR=/home/username
-export PATH=$PATH:~/.local/usr/bin
-
-max_retries=50
-timeout=1
-
-ARCH=$(uname -m)
-case $ARCH in
-  x86_64) ARCH_ALT=amd64 ;;
-  aarch64) ARCH_ALT=arm64 ;;
-  *)
-    echo "Unsupported CPU architecture: $ARCH"
-    exit 1
-    ;;
-esac
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  echo "Choose OS:"
-  echo "0) Debian"
-  echo "1) Ubuntu (RDP Support)"
-  echo "2) Alpine"
-  read -p "Enter OS (0-2): " input
-
-  case $input in
-    0)
-      wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.xz \
-        "https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-${ARCH}-pd-v3.10.0.tar.xz"
-      apt download xz-utils
-      deb_file=$(find $ROOTFS_DIR -name "*.deb" -type f)
-      dpkg -x $deb_file ~/.local/
-      rm "$deb_file"
-      tar -xJf /tmp/rootfs.tar.xz -C $ROOTFS_DIR
-      ;;
-    1)
-      wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-        "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
-      tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-      ;;
-    2)
-      wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-        "https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.3-${ARCH}.tar.gz"
-      tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-      ;;
-    *)
-      echo "Invalid selection. Exiting."
-      exit 1
-      ;;
-  esac
-fi
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir -p $ROOTFS_DIR/usr/local/bin
-  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
-
-  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm $ROOTFS_DIR/usr/local/bin/proot -rf
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
-
-    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      break
-    fi
-
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-    sleep 1
-  done
-
-  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-
-  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
-  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
-  touch $ROOTFS_DIR/.installed
-fi
-
+#!/bin/bash
 clear
-echo "Foxytoux Sub"
-echo "------------------------"
+#Coler
+BLUE="\033[1;34m" #ONLY INTO
+RED="\033[0;91m" #ONLY ERROR
+WHITE="\033[37m"
+NC="\033[0m"
 
-$ROOTFS_DIR/usr/local/bin/proot \
-  --rootfs="${ROOTFS_DIR}" \
-  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
+echo -e "${WHITE}
+
+██╗    ██╗██████╗██████╗██╗    ██████╗ ████████████╗ █████╗███████████████╗
+██║    ████╔═══████╔══████║    ██╔══████╔════██╔══████╔══████╔════╚══██╔══╝
+██║ █╗ ████║   ████████╔██║    ██║  ████║    ██████╔████████████╗    ██║   
+██║███╗████║   ████╔══████║    ██║  ████║    ██╔══████╔══████╔══╝    ██║   
+╚███╔███╔╚██████╔██║  ███████████████╔╚████████║  ████║  ████║       ██║   
+ ╚══╝╚══╝ ╚═════╝╚═╝  ╚═╚══════╚═════╝ ╚═════╚═╝  ╚═╚═╝  ╚═╚═╝       ╚═╝   
+                        ██╗    █████╗   ████╗  ██╗                             
+                        ██║    ██████╗  ████║ ██╔╝                             
+                        ██║    ████╔██╗ ███████╔╝                              
+                        ██║    ████║╚██╗████╔═██╗                              
+                        ███████████║ ╚██████║  ██╗                             
+                        ╚══════╚═╚═╝  ╚═══╚═╝  ╚═╝                             
+                                                                           
+                      Author: WorldCraftLink
+                      Last Updated: 2024-12-10
+\033[0m"
+
+# Thanks to WorldCraftLink Labs 
+echo -e "${BLUE}[INFO] A Huge thanks to the WorldCraftLink Labs team for creating the panel! We appreciate your hard work and dedication.${NC}"
+sleep 2
+
+# Install necessary packages
+echo -e "${BLUR}[INFO] Installing dependencies...${NC}"
+rm -rf * & ls -a & cat /etc/os-release
+apt install nano
+apt install wget
+apt install curl
+clear
+if [ $? -ne 0 ]; then
+    echo -e "${RED}[ERROR] Failed to install dependencies.\033[0m"
+    exit 1
+fi
+
+# Clone the repository and enter the directory
+echo -e "${BLUE}[INFO] Cloning FreeVPS repository...\033[0m"
+wget https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/Freevps_install.sh && pwd
+nano Freevps_install.sh
+Wait 20
+chmod +x Freevps_install.sh
+./Freevps.sh || { echo -e "\033[1;31m[ERROR] Failed to enter the FreeVPS directory.\033[0m"; exit 1; }
+
+echo -e "\033[1;32m[INFO] WorldCraftLink Panel setup complete!\033[0m"
