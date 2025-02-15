6f6c0201b05d855a7698bc853decd85de1bb6a7a
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-15 01:05:51 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -36,7 +36,7 @@ if [ $option -eq 1 ]; then
         read option_freevps
             if [ $option_freevps -eq 1 ]; then
                 sleep 5
-                 git clone https://github.com/itsgamer2o24/Server/FreeVPS.git && sudo bash Server/FreeVPS
+                 https://github.com/itsgamer2o24/Server/tree/main/FreeVPS && sudo bash Freevps.sh
                  #bash <(curl -s http://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/FreeVPS/Freevps.sh)
             elif [ $option_freevps -eq 2 ]; then
                 clear
