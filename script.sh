3fffaf146cf7f14fcf7e6912db2f913b9b6f4117
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-16 00:02:50 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 clear
-
-# Custom 
+#
 NC="\033[0m"
 BOLD="\033[01m"
 DISABLE="\033[02m"
-
 #Coler
 BLUE="\033[1;34m" #ONLY INFO
 RED="\033[0;91m" #ONLY 
 DARKGREY="\033[90m"
 WHITE="\033[37m"
-
-#----------------------------------------------------------------------
 
 echo -e "${WHITE}
 
@@ -35,10 +31,11 @@ echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}Select an option:${NC}"
 echo -e "▶ 1${BOLD}${WHITE} | Install Free VPS${NC}"
 echo -e "▶ 2${BOLD}${WHITE} | Host Server${NC}"
 read option
-
-    if [ $option -eq 1 ]; then
-       clear
-       #bash <(curl https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/InstallSD/FreeVPS_SD.sh)
-else
-  echo -e "${BOLD}${RED}[ERROR] ${DISABLE}Invalid option selected.${NC}" 
-fi
+ 
+if [ $option -eq 1 ]; then #VPS host
+    echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}This create a free vps${NC}"
+    sleep 2
+    bash <(curl https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/InstallSD/FreeVPS_SD.sh)
+   else
+          echo -e "${BOLD}${RED}[ERROR] ${DISABLE}Invalid option selected.${NC}"
+   fi
