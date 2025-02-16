037dca74a78ba65468ebe977a3ee0a2c38628a57
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-16 00:05:08 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/script.sh
+bash <(curl -s https://raw.githubusercontent.com/itsgamer2o24/Server/refs/heads/main/script.sh)
